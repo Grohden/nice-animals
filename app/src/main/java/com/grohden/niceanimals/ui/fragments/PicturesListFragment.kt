cc0088fe89b669dc0a1eb4499995670c5a525831
Gabriel de Oliveira Rohden <gabriel.rohden@outlook.com>
2019-06-29 08:51:24 -0300

    Restore github icon

--- app/src/main/java/com/grohden/niceanimals/ui/fragments/PicturesListFragment.kt
+++ app/src/main/java/com/grohden/niceanimals/ui/fragments/PicturesListFragment.kt
@@ -1,5 +1,7 @@
 package com.grohden.niceanimals.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -10,6 +12,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import com.grohden.niceanimals.AppConstants
+import com.grohden.niceanimals.R
 import com.grohden.niceanimals.base.di.manual.InjectorUtil
 import com.grohden.niceanimals.databinding.FragmentPictureListBinding
 import com.grohden.niceanimals.ui.adapters.PictureAdapter
@@ -44,18 +48,21 @@ class PicturesListFragment : Fragment() {
   }
 
   override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//    inflater.inflate(R.menu.menu_plant_list, menu)
+    inflater.inflate(R.menu.main_menu, menu)
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//    return when (item.itemId) {
-//      R.id.filter_zone -> {
-//        updateData()
-//        true
-//      }
-//      else -> super.onOptionsItemSelected(item)
-//    }
-    return super.onOptionsItemSelected(item)
+    return when (item.itemId) {
+      R.id.menu_github -> {
+        val browserIntent = Intent(
+          Intent.ACTION_VIEW,
+          githubUrl
+        )
+        startActivity(browserIntent)
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
   }
 
   private fun subscribeUi(adapter: PictureAdapter) {
@@ -77,5 +84,9 @@ class PicturesListFragment : Fragment() {
 //        setGrowZoneNumber(9)
 //      }
 //    }
+  }
+
+  companion object {
+    val githubUrl: Uri = Uri.parse(AppConstants.PROJECT_URL)
   }
 }
